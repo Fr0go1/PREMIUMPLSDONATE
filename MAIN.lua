f20268a54f30028368f1a2e59cc5df12e6c22eaf
Fr0go1 <112802612+Fr0go1@users.noreply.github.com>
2023-01-14 15:45:10 +1100

    Update MAIN.lua

--- MAIN.lua
+++ MAIN.lua
@@ -796,7 +796,7 @@ end)
 
 serverHopTab:Toggle("Re-Execute", "Executes the script after server hop\nTurn this off if you use autoexec", getgenv().settings.reex, function(t)
     if t and not exd then
-        queueonteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/heqds/Pls-Donate-Premium/main/hwidobfuscated.lua'))()")
+        queueonteleport("loadstring(game:HttpGet("https://raw.githubusercontent.com/Fr0go1/PREMIUMPLSDONATE/main/MAIN.lua"))()")
         exd = true
     end
     if settingsLock then return end
